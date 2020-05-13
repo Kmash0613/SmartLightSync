aa4c5d604a16c27e8e82d3d3026a34f08e5b5511
Kmash0613 <52874302+Kmash0613@users.noreply.github.com>
2020-05-14 02:44:05 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ var http = require('http');
 var app = express();
 app.use(express.static(__dirname + '/'));
 
-var server = http.createServer(app).listen(80);
-var io = io.listen(server);
+//var server = http.createServer(app).listen(80);
+//var io = io.listen(server);
+
+http.createServer(
+    //function (request, response) {
+    // リクエストを受けると以下のレスポンスを送信する
+    //response.writeHead(200, {'Content-Type': 'text/plain'}); //レスポンスヘッダーに書き込み
+    //response.end('Hello World\n'); // レスポンスボディに書き込み＆レスポンス送信を完了する
+//}
+).listen(process.env.PORT || 8080); //公開ポートで待ち受け
 
 io.sockets.on("connection", function(socket) {
 
