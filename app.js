591acb25eec28a63633abdedc46fa4d4da97b46e
熊谷 周太 <[kumar0613@gmail.com]>
2020-05-14 04:58:37 +0900

    app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var io = require('socket.io');
 var express = require('express');
 var http = require('http');
 var app = express();
-//app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/'));
 
 
 
