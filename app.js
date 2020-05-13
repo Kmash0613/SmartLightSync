a13bc6db27459f8ca1d24a564640c35af1330444
熊谷 周太 <[kumar0613@gmail.com]>
2020-05-14 04:30:25 +0900

    app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-//var io = require('socket.io');
-//var express = require('express');
+var io = require('socket.io');
+var express = require('express');
 var http = require('http');
-//var app = express();
-//app.use(express.static(__dirname + '/'));
+var app = express();
+app.use(express.static(__dirname + '/'));
 
 
 
@@ -13,3 +13,28 @@ http.createServer(
     response.end('Hello World\n'); // レスポンスボディに書き込み＆レスポンス送信を完了する
 }
 ).listen(process.env.PORT || 8080); //公開ポートで待ち受け
+
+//var server = http.createServer(app).listen(80);
+var io = io.listen(server);
+
+io.sockets.on("connection", function(socket) {
+
+    // メッセージ送信（送信者にも送られる）
+    socket.on("C_to_S_message", function(data) {
+        io.sockets.emit("S_to_C_message", {
+            value: data.value
+        });
+    });
+
+    // ブロードキャスト（送信者以外の全員に送信）
+    socket.on("C_to_S_broadcast", function(data) {
+        socket.broadcast.emit("S_to_C_message", {
+            value: data.value
+        });
+    });
+
+    // 切断したときに送信
+    socket.on("disconnect", function() {
+        //    io.sockets.emit("S_to_C_message", {value:"user disconnected"});
+    });
+});
